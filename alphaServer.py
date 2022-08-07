13e4e384f77e8662bd924b95f2ec63ca15026dae
awl.ddn <muhammadawaldin@gmail.com>
2022-08-07 15:10:42 +0800

    Adding multithread counter to count ssh attemp

--- alphaServer.py
+++ alphaServer.py
@@ -6,14 +6,32 @@ from multiprocessing import Value
 # Creating flask object
 app = Flask(__name__)
 
+# Instantiate counter value variable
+counterABC = Value('i', 0)
+counterXYZ = Value('i', 0)
+
+
 # Server routing for different node
 @app.route('/<string:node>')
 def index(node):
     if node == "nodeABC":
-        return {"node": node}
-    else:
-        return {"node": node}
+        # Lock the increment value (ssh attempt) for each node everytime the get request come
+        with counterABC.get_lock():
+            counterABC.value += 1
+            attemps = counterABC.value
+            
+        # Print the metrics on server side
+        print(node + " had " + str(attemps) + " attempt(s)")
 
+        # return success message to client, 
+        # it will not shown to the client, it here only because the return is neccesary
+        return {"message": "SSH success"}
+    else:
+        with counterXYZ.get_lock():
+            counterXYZ.value += 1
+            attemps = counterXYZ.value       
+        print(node + " had " + str(attemps) + " attempt(s)")
+        return {"message": "SSH success"}
 
 # Run flask server
 if __name__ == "__main__":
