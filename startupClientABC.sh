75ece038e7064858e7d65b6a2d2d15d56e8b4535
awl.ddn <muhammadawaldin@gmail.com>
2022-08-08 02:16:55 +0800

    configure server url on each node

--- startupClientABC.sh
+++ startupClientABC.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/BangAwal/fintax-intern-test.git
 cd fintax-intern-test/
+sed -i 's/serverURL/35.225.126.73/' alphaClientABC.sh
 sudo cp alphaClientABC.sh /etc/profile.d/
--- startupClientXYZ.sh
+++ startupClientXYZ.sh
@@ -1,3 +1,4 @@
 git clone https://github.com/BangAwal/fintax-intern-test.git
 cd fintax-intern-test/
+sed -i 's/serverURL/35.225.126.73/' alphaClientXYZ.sh
 sudo cp alphaClientXYZ.sh /etc/profile.d/
